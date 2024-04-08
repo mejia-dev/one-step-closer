6cd7db925d5e7f67ba0f99a31bb27702da07d3c0
gabe <gabrieltucker260@gmail.com>
2024-04-08 14:29:41 -0700

    added packages for routing and edited app.js

--- client/App.js
+++ client/App.js
@@ -1,20 +1,23 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import React from 'react';
+import { NavigationContainer } from '@react-navigation/native';
+import { createStackNavigator } from '@react-navigation/stack';
+import NavBar from './src/components/NavBar';
+import Page1 from './src/components/page1';
+import Page2 from './src/components/page2';
+import Page3 from './src/components/page3';
+
+const Stack = createStackNavigator();
 
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
+    //signin/up here 
+    <NavigationContainer>
+      <Stack.Navigator>
+        <Stack.Screen name="Page1" component={Page1} />
+        <Stack.Screen name="Page2" component={Page2} />
+        <Stack.Screen name="Page3" component={Page3} />
+      </Stack.Navigator>
+      <NavBar />
+    </NavigationContainer>
   );
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
