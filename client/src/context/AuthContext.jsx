9a58a54e57748f73dbc73c9f4ce503c1c57d1293
mejia-dev <aaron.mejia8484@gmail.com>
2024-04-11 13:20:55 -0700

    Add comments with explanations

--- client/src/context/AuthContext.jsx
+++ client/src/context/AuthContext.jsx
@@ -9,18 +9,33 @@ export default AuthContext;
 
 export const AuthProvider = ({ children }) => {
 
+  // this code checks if there are authTokens in local storage. If there are authTokens, it keeps the existing ones. If not, it sets the value to null.
+  const [authTokens, setAuthTokens] = useState(async () => AsyncStorage.getItem('authTokens') ? await AsyncStorage.getItem('authTokens') : null);
+  const [user, setUser] = useState(null);
 
-  const [authTokens, setAuthTokens] = useState(async ()=> AsyncStorage.getItem('authTokens') ? await AsyncStorage.getItem('authTokens') : null);
-  const [user, setUser] = useState(async ()=> AsyncStorage.getItem('authTokens') ? await AsyncStorage.getItem('authTokens') : null);
+  // this code parses the existence of user data on first render. Can't be done inline otherwise the state slice becomes a promise, not a string or null.
+  const firstRenderCheckUser = async () => {
+    const userDataExistence = await AsyncStorage.getItem('authTokens').then((response) => {
+      if (response) {
+        setUser(jwtDecode(response));
+      } else {
+        setUser(null);
+      }
+    });
+  }
+  firstRenderCheckUser();
 
+  
+  // login user. Fetch from the auth api. 
+    // If the response is good, set the local state slices, then add the tokens to Async storage
   const loginUser = async (event) => {
     event.preventDefault();
     const response = await fetch('http://127.0.0.1:8000/authapi/token/', {
       method: 'POST',
       headers: {
-        'Content-Type':'application/json'
+        'Content-Type': 'application/json'
       },
-      body:JSON.stringify({'username':event.target.username.value, 'password':event.target.password.value})
+      body: JSON.stringify({ 'username': event.target.username.value, 'password': event.target.password.value })
     });
     const data = await response.json();
     if (response.status === 200) {
@@ -28,26 +43,28 @@ export const AuthProvider = ({ children }) => {
       setUser(jwtDecode(data.access));
       try {
         AsyncStorage.setItem('authTokens', JSON.stringify(data));
-      } catch(error) {
+      } catch (error) {
         console.log("Error saving tokens to async storage: " + error.message);
       }
     } else {
-      console.log('The API returned error code '+ response.status);
+      console.log('The API returned error code ' + response.status);
     }
   }
 
+  // logout user. Clear local state slices, then remove tokens from async storage
   const logoutUser = () => {
     setAuthTokens(null);
     setUser(null);
     AsyncStorage.removeItem('authTokens');
   }
 
+  // development only. Check for the exist
   const TESTgetUserData = async () => {
     event.preventDefault();
     try {
       const jsonValue = await AsyncStorage.getItem('authTokens');
-      console.log( jsonValue != null ? JSON.parse(jsonValue) : null)
-    } catch(e) {
+      console.log(jsonValue != null ? JSON.parse(jsonValue) : null)
+    } catch (e) {
       // read error
     }
   }
@@ -78,11 +95,11 @@ export const AuthProvider = ({ children }) => {
   // }
 
   let contextData = {
-    user:user,
+    user: user,
     // authTokens:authTokens,
-    loginUser:loginUser,
-    TESTgetUserData:TESTgetUserData
-    // logoutUser:logoutUser,
+    loginUser: loginUser,
+    TESTgetUserData: TESTgetUserData,
+    logoutUser:logoutUser,
   }
 
 
