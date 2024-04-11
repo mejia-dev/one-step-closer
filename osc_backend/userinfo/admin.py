786208ed643a3c4e033baf71b71377cb367e52d6
Teddy Peterschmidt <t.a.peterschmidt@gmail.com>
2024-04-10 17:12:04 -0700

    remove mood model

--- osc_backend/userinfo/admin.py
+++ osc_backend/userinfo/admin.py
@@ -2,5 +2,4 @@ from django.contrib import admin
 
 from .models import * 
 
-admin.site.register(Mood)
 admin.site.register(Goal)
--- osc_backend/userinfo/models.py
+++ osc_backend/userinfo/models.py
@@ -1,14 +1,5 @@
 from django.db import models
 from django.contrib.auth.models import User
-from django.core.validators import MaxValueValidator, MinValueValidator
-
-
-class Mood(models.Model):
-    user = models.ForeignKey(User, on_delete=models.CASCADE)
-    score = models.IntegerField(default = 5, validators=[MinValueValidator(1), MaxValueValidator(10)])
-    time_entered = models.DateTimeField(auto_now_add=True)
-    def __int__(self):
-        return self.score
 
 class Goal(models.Model):
     user = models.ForeignKey(User, on_delete=models.CASCADE)
