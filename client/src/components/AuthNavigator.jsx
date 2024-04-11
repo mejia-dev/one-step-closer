b5e8a8cb206a6c4849705f68dca42a223c51e4fb
mejia-dev <aaron.mejia8484@gmail.com>
2024-04-11 11:04:59 -0700

    Add AuthNavigator

---
+++ client/src/components/AuthNavigator.jsx
@@ -0,0 +1,46 @@
+import React, { useContext, useEffect, useState } from 'react';
+import { StyleSheet, Text, View, Button } from 'react-native';
+import { NavigationContainer } from '@react-navigation/native';
+import { createStackNavigator } from '@react-navigation/stack';
+import Landing from './Landing.jsx';
+import GoalSetup from './GoalSetup';
+import NotificationSetup from './NotificationSetup';
+import Dashboard from './Dashboard';
+import AsyncStorage from '@react-native-async-storage/async-storage';
+
+import LoginForm from './LoginForm.tsx';
+
+const Stack = createStackNavigator();
+
+export default function AuthNavigator() {
+
+  const [isAuthenticated, setIsAuthenticated] = useState(false);
+
+  useEffect(() => {
+    setIsAuthenticated(false);
+  });
+
+  return (
+    <NavigationContainer style={{ flex: 1, flexDirection: 'column' }}>
+      <Stack.Navigator>
+        {isAuthenticated ? (
+          <>
+            <Stack.Screen name="Dashboard" component={Dashboard}
+              options={{ headerShown: false }} />
+            <Stack.Screen name="Goal Setup" component={GoalSetup}
+              options={{ headerShown: false }} />
+            <Stack.Screen name="Notification Setup" component={NotificationSetup}
+              options={{ headerShown: false }} />
+          </>
+        ) : (
+          // <Stack.Screen name="Landing Page" component={Landing}
+          //   options={{ headerShown: false }} />
+            <Stack.Screen name="Login Page" component={LoginForm}
+            options={{ headerShown: false }} />
+        )}
+      </Stack.Navigator>
+      {/* <MoodGraph /> */}
+
+    </NavigationContainer>
+  );
+}
