4b28da01c6c357a9e6357b6c3faa1228a98bd947
mejia-dev <aaron.mejia8484@gmail.com>
2024-04-10 22:59:36 -0700

    Update View to use rest framework Response and decorator

--- osc_backend/userinfo/authapi/views.py
+++ osc_backend/userinfo/authapi/views.py
@@ -1,9 +1,15 @@
 from django.http import JsonResponse
+# from rest_framework import permissions
+from rest_framework.response import Response
+from rest_framework.decorators import api_view, permission_classes
+# from rest_framework.permissions import IsAuthenticated
 
+
+@api_view(['GET'])
 def getRoutes(request):
     routes = [
         '/authapi/token',
         '/authapi/token/refresh',
     ]
 
-    return JsonResponse(routes, safe=False)
+    return Response(routes)
