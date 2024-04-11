71eadab8de84fc316bc53d16854329a66231f99d
mejia-dev <aaron.mejia8484@gmail.com>
2024-04-11 14:09:22 -0700

    Protect the getGoals view

--- osc_backend/userinfo/authapi/views.py
+++ osc_backend/userinfo/authapi/views.py
@@ -1,8 +1,8 @@
 from django.http import JsonResponse
-# from rest_framework import permissions
+from rest_framework import permissions
 from rest_framework.response import Response
 from rest_framework.decorators import api_view, permission_classes
-# from rest_framework.permissions import IsAuthenticated
+from rest_framework.permissions import IsAuthenticated
 from rest_framework_simplejwt.serializers import TokenObtainPairSerializer
 from rest_framework_simplejwt.views import TokenObtainPairView
 
@@ -35,6 +35,7 @@ def getRoutes(request):
     return Response(routes)
 
 @api_view(['GET'])
+@permission_classes([IsAuthenticated])
 def getGoals(request):
     goals = Goal.objects.all()
     serializer = GoalSerializer(goals, many=True)
