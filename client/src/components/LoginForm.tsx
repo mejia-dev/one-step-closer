38bf266126f031f3a073cd7e183c0e551b3dd797
mejia-dev <aaron.mejia8484@gmail.com>
2024-04-15 08:36:49 -0700

    Update LoginForm styling

--- client/src/components/LoginForm.tsx
+++ client/src/components/LoginForm.tsx
@@ -1,18 +1,45 @@
-import React, {useContext} from 'react'
+import React, { useContext } from 'react'
 import AuthContext from '../context/AuthContext'
-import { Text, View } from 'react-native'
+import { Text, View, StyleSheet } from 'react-native'
 
 const LoginForm = () => {
-    const {loginUser} = useContext(AuthContext);
+    const { loginUser } = useContext(AuthContext);
     return (
-        <View>
-            <form onSubmit={loginUser}>
-                <input type="text" name="username" placeholder="Enter Username" />
-                <input type="password" name="password" placeholder="Enter Password" />
-                <input type="submit"/>
+        <View style={styles.background}>
+            <Text style={styles.header}>Please Log In</Text>
+            <form onSubmit={loginUser} style={styles.form}>
+                <input type="text" name="username" placeholder="Enter Username" style={styles.formItem} />
+                <input type="password" name="password" placeholder="Enter Password" style={styles.formItem} />
+                <input type="submit" style={styles.formItem} />
             </form>
         </View>
     )
 }
+
+const styles = StyleSheet.create({
+    background: {
+        justifyContent: 'center',
+        backgroundColor: '#00365C',
+        position: 'absolute',
+        left: 0,
+        right: 0,
+        top: 0,
+        height: '100%',
+    },
+    form: {
+        display: "flex",
+        flexDirection: "column"
+    },
+    formItem: {
+        padding: 10,
+        margin: 5,
+        borderRadius: 5,
+    },
+    header: {
+        fontSize: 30,
+        color: "white",
+        textAlign: "center"
+    }
+});
 
 export default LoginForm;
